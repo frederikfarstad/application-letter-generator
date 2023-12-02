41ee3d1d5c77185d0ceb0756a6fb48592eed9fe9
Frederik Andreas Brunvoll Farstad <frederikfarstad@gmail.com>
2023-12-02 14:14:16 +0100

    feat: costumize template-letter

--- main.py
+++ main.py
@@ -7,29 +7,36 @@ import os
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-# Open the resume file
-with open("resume.txt", "r", encoding="utf8") as file:
-    resume = file.read()
-
 # Create a cover letter for a job advertisement
-def create_coverLetter(jobAd, resume):
+def create_coverLetter(jobAd, resume, template):
     
-    rules = "Reference my resume in the cover letter. Reference the company in the cover letter. Tone: conversational, spartan, use less corporate jargon, friendly, professional, confident. Include information about the volutarily work from the resume. Language: Norwegian."
-    content = "This is my resume: " + resume + ". Write a cover letter for the following job adveritsement: " + jobAd + ". " + rules
+    language = "Norwegian"
+    rules = "You are a cover letter generator. Use the provided template cover letter and adjust it to the job advert. Reference the resume. Make it ATS-friendly. Language: "+ language + "."
+    content = "This is my resume: " + resume + ".\n This is the cover letter template: " + template + ".\n Write a cover letter for the following job adveritsement: " + jobAd + ". "
 
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
+      {"role": "system", "content": rules},
       {"role": "user", "content": content}
     ]
     )
 
     return completion.choices[0].message.content, completion.usage.completion_tokens, completion.usage.prompt_tokens, completion.usage.total_tokens
 
+
+# Open the resume file
+with open("resume.txt", "r", encoding="utf8") as file:
+    resume = file.read()
+
+with open("template.txt", "r", encoding="utf8") as file:
+    template = file.read()
+
 print("\n")
 print("Starting cover letter generation...\n")
 
 tokenSum = 0
+totalCost = 0
 startTime = time.time()
 
 #Parse through the jobAds folder
@@ -45,12 +52,14 @@ for filename in os.listdir('jobAds'):
     print("Generating application for " + filename.replace(".txt", "").capitalize())
 
     # Generate the cover letter
-    coverLetter, completionTokens, promtTokens, totalTokens = create_coverLetter(jobAd, resume)
+    coverLetter, completionTokens, promtTokens, totalTokens = create_coverLetter(jobAd, resume, template)
 
     print("Completion tokens: " + str(completionTokens) + ", prompt tokens: " + str(promtTokens) + ", total tokens: " + str(totalTokens))
     print("Time used: " + str(round(time.time() - startGenTime, 2)) + " seconds\n")
+    print("Cost ≈ " + str(round(totalTokens*0.0000030837, 5)) + "$\n")
 
     tokenSum += totalTokens
+    totalCost += totalTokens*0.0000030837
 
     #add the generated cover letter to a new txt file in the coverLetters folder:
     with open('coverLetters/' + filename, 'w', encoding="utf8") as file:
@@ -61,6 +70,7 @@ endTime = round(time.time() - startTime, 2)
 # Print the statistics for the generation of the cover letters
 print("Total tokens used: " + str(tokenSum))
 print("Total time used: " + str(endTime) + " seconds")
+print("Total cost ≈ " + str(round(totalCost, 5)) + "$")
 print("Average tokens per application: " + str(round(tokenSum/len(os.listdir('jobAds')), 2)) + " tokens")
 print("Average time per application: " + str(round(endTime/len(os.listdir('jobAds')), 2)) + " seconds")
 print("\n")
